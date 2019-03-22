42226e88c1c8e1479f86358b298c9a088e626281
Ihsan Batughan Yilmaz <cameloper@cameloper.com>
2019-03-22 13:12:25 +0100

    - Removed Group/Material   + Added PartManager

--- src/edu/kit/informatik/Part.java
+++ src/edu/kit/informatik/Part.java
@@ -1,5 +1,34 @@
 package edu.kit.informatik;
 
-interface Part {
-    String getId();
+import java.util.HashMap;
+
+class Part {
+    private String id;
+    private HashMap<String, Integer> children;
+
+    public Part(String id, HashMap<String, Integer> children) {
+        this.id = id;
+        this.children = children;
+    }
+
+    /**
+     * Getter of Id
+     *
+     * @return value of private variable Id
+     */
+    String getId() {
+        return id;
+    }
+
+    Type getType() {
+        if (children.isEmpty())
+            return Type.COMPONENT;
+        else
+            return Type.ASSEMBLY;
+    }
+
+    enum Type {
+        ASSEMBLY,
+        COMPONENT
+    }
 }
---
+++ src/edu/kit/informatik/PartManager.java
@@ -0,0 +1,8 @@
+package edu.kit.informatik;
+
+import java.util.ArrayList;
+
+public class PartManager {
+    public static PartManager main;
+    private ArrayList<Part> parts;
+}
