e1efd6635ec4ba4cc7d0a5b78a54c29888385e41
Ihsan Batughan Yilmaz <cameloper@cameloper.com>
2019-03-22 05:47:19 +0100

    + Added the command enum cases

--- src/edu/kit/informatik/Command.java
+++ src/edu/kit/informatik/Command.java
@@ -4,5 +4,29 @@ enum Command {
     /**
      * Adds a new Group object
      */
-    ADD_ASSEMBLY
+    ADD_ASSEMBLY,
+    /**
+     * Prints contents of a Group object
+     */
+    PRINT_ASSEMBLY,
+    /**
+     * Lists all child-groups of a Group
+     */
+    GET_ASSEMBLIES,
+    /**
+     * Lists all materials of a Group
+     */
+    GET_COMPONENTS,
+    /**
+     * Adds a new part
+     */
+    ADD_PART,
+    /**
+     * Removes a specific part
+     */
+    REMOVE_PART,
+    /**
+     * Quits the app
+     */
+    QUIT
 }
